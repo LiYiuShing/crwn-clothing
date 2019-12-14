d780f75fe07c9b5ae0162c3674895bb37c080e78
LiYiuShing <44675791+LiYiuShing@users.noreply.github.com>
2019-12-14 16:48:46 +0800

    updating app this a shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/hompage.component'
+import ShopPage from './pages/shop/shop.component.jsx'
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
