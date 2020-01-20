8d418835417bf0106659aec47b6fce629dcc83a1
LiYiuShing <44675791+LiYiuShing@users.noreply.github.com>
2020-01-20 23:56:12 +0800

    added express server to help backend stript payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_3xfJ6D77nWXarMFUdi1s2jXA00ajFsoUJ5';
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment.'
+            )
+        })
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Li Yiu Shing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
